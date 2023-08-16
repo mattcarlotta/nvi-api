31b396aed437301bd72e1270cbd3842057c30a58
Matt <matt@mattcarlotta.com>
2023-08-15 19:56:29 -0700

    chore(middleware): simplify cookie check middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -12,20 +12,17 @@ import (
 func CookieSession(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 		cookie, err := req.Cookie("SESSION_TOKEN")
-		if err != nil {
-			utils.SendErrorResponse(res, http.StatusUnauthorized, "You must be logged in order to do that!")
-			return
-		} else if len(cookie.Value) == 0 {
+		if err != nil || len(cookie.Value) == 0 {
 			utils.SendErrorResponse(res, http.StatusUnauthorized, "You must be logged in order to do that!")
 			return
 		}
 
-		data, err := utils.ValidateSessionToken(cookie.Value)
+		token, err := utils.ValidateSessionToken(cookie.Value)
 		if err != nil {
 			utils.SendErrorResponse(res, http.StatusUnauthorized, err.Error())
 			return
 		}
-		ctx := context.WithValue(req.Context(), "userSessionId", data.UserId)
+		ctx := context.WithValue(req.Context(), "userSessionId", token.UserId)
 
 		next.ServeHTTP(res, req.WithContext(ctx))
 	})

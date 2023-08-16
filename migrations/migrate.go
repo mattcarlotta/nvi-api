4859ea24ecec81027f475fd074ce7c0842738282
Matt <matt@mattcarlotta.com>
2023-08-15 20:03:07 -0700

    chore(migration): log and exit on migration errors

--- migrations/migrate.go
+++ migrations/migrate.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/mattcarlotta/nvi-api/database"
 	"github.com/mattcarlotta/nvi-api/models"
 )
 
 func main() {
 	var db = database.GetDB()
-	db.AutoMigrate(&models.User{})
-	db.AutoMigrate(&models.Environment{})
+	var err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatalf("Unable to migrate User model: %s", err.Error())
+	}
+	err = db.AutoMigrate(&models.Environment{})
+	if err != nil {
+		log.Fatalf("Unable to migrate Environment model: %s", err.Error())
+	}
 	fmt.Println("🎉 Migration complete")
 }

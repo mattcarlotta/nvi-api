6380f74e3625e39575df6fb71de72e37ac3e2c46
Matt <matt@mattcarlotta.com>
2023-08-23 17:58:39 -0700

    chore: remove pq dep

--- models/secret.go
+++ models/secret.go
@@ -4,16 +4,14 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	// "github.com/lib/pq"
 	"github.com/mattcarlotta/nvi-api/utils"
 	"gorm.io/gorm"
 )
 
 type Secret struct {
-	ID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
-	UserId uuid.UUID `gorm:"type:uuid" json:"userId"`
-	User   User      `gorm:"foreignKey:UserId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
-	// EnvironmentIds pq.StringArray `gorm:"type:text[]" json="-"`
+	ID           uuid.UUID     `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
+	UserId       uuid.UUID     `gorm:"type:uuid" json:"userId"`
+	User         User          `gorm:"foreignKey:UserId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 	Environments []Environment `gorm:"many2many:environment_secrets;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Name         string        `gorm:"type:varchar(255);not null" json:"name"`
 	Content      string        `gorm:"type:varchar(10000);not null" json:"content"`

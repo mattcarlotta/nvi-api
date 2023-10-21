c1234c94e6bab0798443da783d1e9edcc4d8f9b2
Matt Carlotta <matt@mattcarlotta.com>
2023-10-20 18:38:57 -0700

    refactor: create reusable custom email struct for sending emails

--- utils/email.go
+++ utils/email.go
@@ -5,64 +5,97 @@ import (
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-func SendEmail(p *mail.Personalization, templateID string, name string, email string) error {
-	m := mail.NewV3Mail()
-	m.SetTemplateID(templateID)
+type CustomEmail struct {
+	M          *mail.SGMailV3
+	P          *mail.Personalization
+	Name       string
+	Address    string
+	TemplateID string
+}
 
-	fromEmailAddress := mail.NewEmail("nvi", GetEnv("EMAIL_ADDRESS"))
-	m.SetFrom(fromEmailAddress)
+func (email *CustomEmail) setDefaults() {
+	email.M.SetTemplateID(email.TemplateID)
+	email.M.SetFrom(mail.NewEmail("nvi", GetEnv("EMAIL_ADDRESS")))
 
-	toEmailAddresses := []*mail.Email{mail.NewEmail(name, email)}
-	p.AddTos(toEmailAddresses...)
+	toEmailAddresses := []*mail.Email{mail.NewEmail(email.Name, email.Address)}
+	email.P.AddTos(toEmailAddresses...)
+	email.P.SetDynamicTemplateData("name", email.Name)
+	email.P.SetDynamicTemplateData("unsubscribe", GetEnv("CLIENT_HOST")+"/settings/")
+	email.P.SetDynamicTemplateData("unsubscribe_preferences", GetEnv("CLIENT_HOST")+"/settings/")
+}
 
-	p.SetDynamicTemplateData("name", name)
-	p.SetDynamicTemplateData("unsubscribe", GetEnv("CLIENT_HOST")+"/settings/")
-	p.SetDynamicTemplateData("unsubscribe_preferences", GetEnv("CLIENT_HOST")+"/settings/")
+func (email *CustomEmail) setTemplateData(property string, value string) {
+	email.P.SetDynamicTemplateData(property, value)
+}
 
-	m.AddPersonalizations(p)
+func (email *CustomEmail) save() {
+	email.M.AddPersonalizations(email.P)
+}
 
+func (email *CustomEmail) send() error {
 	request := sendgrid.GetRequest(GetEnv("SEND_GRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
 	request.Method = "POST"
-	request.Body = mail.GetRequestBody(m)
+	request.Body = mail.GetRequestBody(email.M)
 	_, err := sendgrid.API(request)
 	return err
 }
 
-func SendAccountVerificationEmail(name string, email string, token string) error {
+func SendAccountVerificationEmail(name string, address string, token string) error {
 	if GetEnv("IN_TESTING") != "false" {
 		return nil
 	}
 
-	templateID := GetEnv("SEND_GRID_VERIFICATION_TEMPLATE_ID")
+	email := CustomEmail{
+		M:          mail.NewV3Mail(),
+		P:          mail.NewPersonalization(),
+		Name:       name,
+		Address:    address,
+		TemplateID: GetEnv("SEND_GRID_VERIFICATION_TEMPLATE_ID"),
+	}
 
-	p := mail.NewPersonalization()
-	p.SetDynamicTemplateData("verify_link", GetEnv("CLIENT_HOST")+"/verify?token="+token)
+	email.setDefaults()
+	email.setTemplateData("verify_link", GetEnv("CLIENT_HOST")+"/verify?token="+token)
+	email.save()
 
-	return SendEmail(p, templateID, name, email)
+	return email.send()
 }
 
-func SendPasswordResetEmail(name string, email string, token string) error {
+func SendPasswordResetEmail(name string, address string, token string) error {
 	if GetEnv("IN_TESTING") != "false" {
 		return nil
 	}
 
-	templateID := GetEnv("SEND_GRID_PASSWORD_RESET_TEMPLATE_ID")
+	email := CustomEmail{
+		M:          mail.NewV3Mail(),
+		P:          mail.NewPersonalization(),
+		Name:       name,
+		Address:    address,
+		TemplateID: GetEnv("SEND_GRID_PASSWORD_RESET_TEMPLATE_ID"),
+	}
 
-	p := mail.NewPersonalization()
-	p.SetDynamicTemplateData("reset_password_link", GetEnv("CLIENT_HOST")+"/reset-password?token="+token)
+	email.setDefaults()
+	email.setTemplateData("reset_password_link", GetEnv("CLIENT_HOST")+"/reset-password?token="+token)
+	email.save()
 
-	return SendEmail(p, templateID, name, email)
+	return email.send()
 }
 
-func SendPasswordResetConfirmationEmail(name string, email string) error {
+func SendPasswordResetConfirmationEmail(name string, address string) error {
 	if GetEnv("IN_TESTING") != "false" {
 		return nil
 	}
 
-	templateID := GetEnv("SEND_GRID_PASSWORD_RESET_CONFIRMATION_TEMPLATE_ID")
+	email := CustomEmail{
+		M:          mail.NewV3Mail(),
+		P:          mail.NewPersonalization(),
+		Name:       name,
+		Address:    address,
+		TemplateID: GetEnv("SEND_GRID_PASSWORD_RESET_CONFIRMATION_TEMPLATE_ID"),
+	}
 
-	p := mail.NewPersonalization()
-	p.SetDynamicTemplateData("contact_us_link", GetEnv("CONTACT_US_LINK"))
+	email.setDefaults()
+	email.setTemplateData("contact_us_link", GetEnv("CONTACT_US_LINK"))
+	email.save()
 
-	return SendEmail(p, templateID, name, email)
+	return email.send()
 }
